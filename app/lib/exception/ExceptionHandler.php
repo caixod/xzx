8014adc56419f6952e2ae80646a5d8133f720ae1
caixod <375197028@qq.com>
2018-04-11 17:55:46 +0800

    411-3

--- app/lib/exception/ExceptionHandler.php
+++ app/lib/exception/ExceptionHandler.php
@@ -27,20 +27,26 @@ class ExceptionHandler extends Handle
             //下面不是自定义异常，是服务器异常不是我们想要的，需要记录日志
             // 如果是服务器未处理的异常，将http状态码设置为500，并记录日志
             if(config('app_debug')){
+
                 // 调试状态下需要显示TP默认的异常页面，因为TP的默认页面
                 // 很容易看出问题
                 return parent::render($e);//调回原来的render方法
+                /**
+                 * 要是开启了xdebug，就是开发模式，没必要记录日志了，如需记录日志修改一下逻辑！！！
+                 */
             }else{
                 //没开启调试模式的异常,一般是显示页面错误，不显示详细信息，api的话还是返回下面数据然后记录日志这样才合理
                 $data['code'] = '500';
                 $data['msg'] = '服务器内部错误';
             }
-
+            /**
+             * 上线状态记录日志，方便查找问题
+             */
             Log::init([
                 'type'  =>  'File',
                 'path'  =>  LOG_PATH,
             ]);
-            Log::write('6666');
+            Log::write($e->getMessage());
 
 //            $this->recordErrorLog($e);
         }
